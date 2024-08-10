cb374565c02b063656960edba5dd188b6fec9541
user <user@example.com>
2024-08-10 12:45:12 +0300

    add custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractBaseUser):
+    username = None
+
+    email = models.EmailField(verbose_name='e-mail', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
