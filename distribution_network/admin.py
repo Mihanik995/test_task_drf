2c9b81fafdaf39529924e6436e9bd28d1fa5ac00
user <user@example.com>
2024-08-10 18:03:50 +0300

    add models to admin platform

--- distribution_network/admin.py
+++ distribution_network/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from distribution_network.models import Product, DistributionNod
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'model', 'release_date')
+
+
+@admin.action(description="Write off debt from selected providers")
+def write_off_dept(modeladmin, request, queryset):
+    queryset.update(dept=0)
+
+
+@admin.register(DistributionNod)
+class DistribNodAdmin(admin.ModelAdmin):
+    list_display = ('title', 'email', 'country', 'city', 'street', 'building',
+                    'dept', 'creation_datetime')
+    filter_horizontal = ('products',)
+    actions = [write_off_dept]
+    search_fields = ('city',)
