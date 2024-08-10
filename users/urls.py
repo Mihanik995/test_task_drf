7f22841bbe2b9837a27aafc965762954e760c6ea
user <user@example.com>
2024-08-10 12:45:25 +0300

    add user endpoints

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('', include('djoser.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
